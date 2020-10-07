e0292d31982f1405c8f97317a50b859d64f25cbf
zesha5816 <zesha5816@wrdsb.ca>
2020-10-07 14:29:02 +0000

    finished the example

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *program so that instead of printing a single row of stars, it prints a box of stars instead!
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//output the message to the user
+		System.out.println("Please enter a number between 1 and 10");
+		//get input form user
+		int userInput = input.nextInt();
+		//increases value of i by an increment of 1 and keeps going until is reaches the userInput
+		for (int i = 0; i < userInput; i++){
+			//insert 5 stars per line
+			for (int j=0; j < userInput; j++ ){
+				System.out.print("*");
+			}
+			//move one line over 
+			System.out.println("");
+		}
   }
 }
